3ae2c0a960bb40fa15b80dafa1a135ec5acb936e
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-03-27 06:24:43 +0500

    add User Service: add, login methods

--- src/main/java/uz/shaftoli/education/entity/UserEntity.java
+++ src/main/java/uz/shaftoli/education/entity/UserEntity.java
@@ -14,7 +14,7 @@ public class UserEntity extends BaseEntity {
     private String name;
     private String surname;
     @Column(unique = true, nullable = false)
-    private String email;
+    private String phoneNumber;
     private String password;
     @Enumerated(EnumType.STRING)
     private UserRole role;
