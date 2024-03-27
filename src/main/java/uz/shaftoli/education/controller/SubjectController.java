f0e3b3400d880997a36280b9ac3045a94ff3ad19
JahongirDevelop <sjahongir221@gmail.com>
2024-03-27 05:09:48 +0500

    Add controller, service and repository dor Subject entity

--- src/main/java/uz/shaftoli/education/controller/SubjectController.java
+++ src/main/java/uz/shaftoli/education/controller/SubjectController.java
@@ -3,9 +3,11 @@ package uz.shaftoli.education.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uz.shaftoli.education.service.SubjectService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/subject")
 public class SubjectController {
+    private final SubjectService subjectService;
 }
