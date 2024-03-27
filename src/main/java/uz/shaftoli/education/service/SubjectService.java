ae85067e785bbef4a2c9bf1ae53422c1fda45b08
JahongirDevelop <sjahongir221@gmail.com>
2024-03-27 05:40:44 +0500

    added dto for subject

--- src/main/java/uz/shaftoli/education/service/SubjectService.java
+++ src/main/java/uz/shaftoli/education/service/SubjectService.java
@@ -1,13 +1,19 @@
 package uz.shaftoli.education.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import uz.shaftoli.education.entity.Subject;
 import uz.shaftoli.education.repository.SubjectRepository;
 
 @Service
 @RequiredArgsConstructor
 public class SubjectService {
     private final SubjectRepository subjectRepository;
+
+
+
 
 
 }
