a894d77dddfb5fc995b08ef3ef8c44d84a981774
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-03-29 03:30:14 +0500

    Add AuthController.java and some methods

--- src/main/java/uz/rivoj/education/service/UserService.java
+++ src/main/java/uz/rivoj/education/service/UserService.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import uz.rivoj.education.dto.request.LoginDTO;
-import uz.rivoj.education.dto.request.UserCreateDTO;
+import uz.rivoj.education.dto.request.LoginRequest;
+import uz.rivoj.education.dto.request.UserCreateRequest;
+import uz.rivoj.education.dto.response.UserResponse;
 import uz.rivoj.education.exception.DataNotFoundException;
 import uz.rivoj.education.entity.UserEntity;
 import uz.rivoj.education.entity.UserRole;
@@ -19,21 +20,21 @@ public class UserService {
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
-    public UserEntity add(UserCreateDTO userDto) {
+    public UserResponse add(UserCreateRequest userDto) {
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
         userEntity.setRole(UserRole.STUDENT);
         userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
-        return userRepository.save(userEntity);
+        return modelMapper.map(userRepository.save(userEntity), UserResponse.class);
     }
 
-    public UserEntity login(LoginDTO login) {
+    public UserResponse login(LoginRequest login) {
         UserEntity userEntity = userRepository.findUserEntityByPhoneNumber(login.getPhoneNumber())
                 .orElseThrow(
                         () -> new DataNotFoundException("user not found")
                 );
 
         if(passwordEncoder.matches(login.getPassword(), userEntity.getPassword())) {
-            return userEntity;
+            return modelMapper.map(userEntity, UserResponse.class);
         }
         throw new WrongPasswordException("password didn't match");
     }
