1150fab5b2d6118ce075898b4159c766ea32cf0f
Ulug'bek <kimsanov0717@gmail.com>
2024-03-30 22:34:54 +0500

    Attendance crud

--- src/main/java/uz/rivoj/education/controller/AttendanceController.java
+++ src/main/java/uz/rivoj/education/controller/AttendanceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/lesson")
+@RequestMapping("api/v1/attendance")
 public class AttendanceController {
 
 }
