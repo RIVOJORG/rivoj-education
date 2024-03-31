2dab5e3caf2f9259e8aa44e9224cf094f3cd404f
JahongirDevelop <sjahongir221@gmail.com>
2024-03-31 18:36:05 +0500

    added controller for Comment

--- src/main/java/uz/rivoj/education/controller/CommentController.java
+++ src/main/java/uz/rivoj/education/controller/CommentController.java
@@ -22,20 +22,20 @@ public class CommentController {
     public ResponseEntity<CommentResponse> createComment(@RequestBody CommentCreateRequest createRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.create(createRequest));
     }
-//
-//    @DeleteMapping("/delete-comment{commentId}")
-//    public ResponseEntity<String> deleteComment(@PathVariable UUID commentId){
-//        return ResponseEntity.status(200).body(commentService.delete(commentId));
-//    }
-//
-//    @GetMapping("/get-all")
-//    public List<CommentResponse> getAll(){
-//        return commentService.getAll();
-//    }
-//
-//    @GetMapping("get-comment/{id}")
-//    public CommentEntity getCommentById(@PathVariable UUID id) {
-//        return commentService.getComment(id);
-//    }
+
+    @DeleteMapping("/delete-comment{commentId}")
+    public ResponseEntity<String> deleteComment(@PathVariable UUID commentId){
+        return ResponseEntity.status(200).body(commentService.delete(commentId));
+    }
+
+    @GetMapping("/get-all")
+    public List<CommentResponse> getAll(){
+        return commentService.getAll();
+    }
+
+    @GetMapping("get-comment/{id}")
+    public CommentEntity getCommentById(@PathVariable UUID id) {
+        return commentService.getComment(id);
+    }
 
 }
