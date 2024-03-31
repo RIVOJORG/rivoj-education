fd3cd9d52e7e34360dc33a911f35cba76a60c0db
JahongirDevelop <sjahongir221@gmail.com>
2024-03-31 08:34:57 +0500

    added commentController

---
+++ src/main/java/uz/rivoj/education/controller/CommentController.java
@@ -0,0 +1,13 @@
+package uz.rivoj.education.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/comment")
+public class CommentController {
+
+}
