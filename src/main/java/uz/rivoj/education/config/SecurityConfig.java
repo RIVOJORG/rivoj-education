49415bbc4e4f8c7c72599e171f404aea273c2e87
Ulug'bek <kimsanov0717@gmail.com>
2024-03-31 12:26:19 +0500

    Swagger configured

--- src/main/java/uz/rivoj/education/config/SecurityConfig.java
+++ src/main/java/uz/rivoj/education/config/SecurityConfig.java
@@ -1,50 +1,51 @@
-package uz.rivoj.education.config;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import uz.rivoj.education.service.AuthService;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-    private final AuthService authService;
-    private final PasswordEncoder passwordEncoder;
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requestConfigurer -> {
-                    requestConfigurer
-                            .requestMatchers("/auth/**").permitAll()
-                            /*
-                             *
-                             * yo'llarni yozish kerak
-                             *
-                             */
-                            .requestMatchers("").hasRole("TEACHER")
-                            .requestMatchers("").hasRole("STUDENT")
-                            .anyRequest().authenticated();
-                }).httpBasic(Customizer.withDefaults())
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity httpSecurity) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder
-                = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(authService)
-                .passwordEncoder(passwordEncoder);
-        return authenticationManagerBuilder.build();
-    }
-}
+//package uz.rivoj.education.config;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import uz.rivoj.education.service.AuthService;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//    private final AuthService authService;
+//    private final PasswordEncoder passwordEncoder;
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(requestConfigurer -> {
+//                    requestConfigurer
+////                            .requestMatchers("/auth/**").permitAll()
+//                            .requestMatchers("/api/**").permitAll()
+//                            /*
+//                             *
+//                             * yo'llarni yozish kerak
+//                             *
+//                             */
+////                            .requestMatchers("").hasRole("TEACHER")
+////                            .requestMatchers("").hasRole("STUDENT")
+//                            .anyRequest().authenticated();
+//                }).httpBasic(Customizer.withDefaults())
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authManager(HttpSecurity httpSecurity) throws Exception {
+//        AuthenticationManagerBuilder authenticationManagerBuilder
+//                = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+//        authenticationManagerBuilder.userDetailsService(authService)
+//                .passwordEncoder(passwordEncoder);
+//        return authenticationManagerBuilder.build();
+//    }
+//}
