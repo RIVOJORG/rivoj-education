6d9f515c7760ef58524269c04e63857a7dd2f8ca
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-03-31 08:38:07 +0500

    added service and repository

---
+++ src/main/java/uz/rivoj/education/service/CommentService.java
@@ -0,0 +1,10 @@
+package uz.rivoj.education.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
