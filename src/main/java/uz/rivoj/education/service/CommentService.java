1074d5823b1917a9821d53734ea6c4b0491625fa
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-03-31 10:29:46 +0500

    added and changed some methods in Module, Comment, Lesson services

--- src/main/java/uz/rivoj/education/service/CommentService.java
+++ src/main/java/uz/rivoj/education/service/CommentService.java
@@ -1,10 +1,41 @@
 package uz.rivoj.education.service;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uz.rivoj.education.dto.request.CommentCreateRequest;
+import uz.rivoj.education.dto.response.CommentResponse;
+import uz.rivoj.education.entity.CommentEntity;
+import uz.rivoj.education.entity.LessonEntity;
+import uz.rivoj.education.repository.CommentRepository;
+import uz.rivoj.education.repository.LessonRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final LessonRepository lessonRepository;
+    private final ModelMapper modelMapper;
+
+    public CommentResponse create(CommentCreateRequest createRequest) {
+        LessonEntity lessonEntity = lessonRepository.findById(createRequest.getLessonId())
+                .orElseThrow(() -> new ClassCastException("Lesson not found with ID: " + createRequest.getLessonId()));
+
+
+        CommentEntity commentEntity = modelMapper.map(createRequest, CommentEntity.class);
+        commentEntity.setLesson(lessonEntity);
+        CommentEntity saved = commentRepository.save(commentEntity);
+        return modelMapper.map(saved, CommentResponse.class);
+
+//        LessonEntity lessonEntity = lessonRepository.findById(createRequest.getLessonId())
+//                .orElseThrow(() -> new ClassCastException("Lesson not found with ID: " + createRequest.getLessonId()));
+//
+//        CommentEntity mapped = modelMapper.map(createRequest, CommentEntity.class);
+//        mapped.setLesson(lessonEntity);
+//
+//         commentRepository.save(mapped);
+//        return modelMapper.map(createRequest, CommentResponse.class);
+
+    }
 
 }
