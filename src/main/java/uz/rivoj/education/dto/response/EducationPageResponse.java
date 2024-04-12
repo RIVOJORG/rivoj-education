c43b3f6e53c6832458f70487ce0a547774fd302b
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-04-12 14:39:41 +0500

    add BestStudentResponse, EducationPageResponse, HomePageResponse

---
+++ src/main/java/uz/rivoj/education/dto/response/EducationPageResponse.java
@@ -0,0 +1,24 @@
+package uz.rivoj.education.dto.response;
+
+
+import lombok.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class EducationPageResponse {
+
+    private Integer countModules;
+    private Integer currentModule;
+    private Integer countLessons;
+    private Integer currentLesson;
+    private Boolean isLessonOver;
+    private Integer coins;
+    private Integer totalScores;
+    private List<BestStudentResponse> bestStudentsOfLessonResponse;
+
+}
