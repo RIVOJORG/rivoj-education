73527051db68fef0a05f352121ab8c7667ce1bb0
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-04-19 23:18:15 +0500

    Add function getScoresListOfModule to AttendanceRepository Done HomePageResponse (MyProgress function)

--- src/main/java/uz/rivoj/education/repository/AttendanceRepository.java
+++ src/main/java/uz/rivoj/education/repository/AttendanceRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.rivoj.education.dto.response.AttendanceResponse;
 import uz.rivoj.education.entity.AttendanceEntity;
+import uz.rivoj.education.entity.ModuleEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,10 @@ import java.util.UUID;
 @Repository
 public interface AttendanceRepository extends JpaRepository<AttendanceEntity, UUID> {
 
+    List<AttendanceEntity> findAttendanceEntitiesByStudentIdAndLessonEntity_Module(
+            UUID student_id,
+            ModuleEntity module
+    );
     List<AttendanceEntity> findAllByStudentId(UUID userId);
     Optional<AttendanceResponse> findByStudentAndLessonId();
 }
