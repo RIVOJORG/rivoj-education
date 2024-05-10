50ca54dae219b0683c6716d2cf844e5c4a437fb0
JahongirDevelop <sjahongir221@gmail.com>
2024-05-10 22:21:28 +0500

    discount

--- src/main/java/uz/rivoj/education/controller/ProgressController.java
+++ src/main/java/uz/rivoj/education/controller/ProgressController.java
@@ -1,19 +1,24 @@
 package uz.rivoj.education.controller;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.rivoj.education.dto.request.DiscountRequest;
+import uz.rivoj.education.dto.response.DiscountResponse;
 import uz.rivoj.education.dto.response.HomePageResponse;
 import uz.rivoj.education.dto.response.RankingPageResponse;
+import uz.rivoj.education.service.DiscountService;
 import uz.rivoj.education.service.ProgressService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/progress")
 public class ProgressController {
     private final ProgressService progressService;
+    private final DiscountService discountService;
     @GetMapping("/get-progress/{phoneNumber}")
     public HomePageResponse getProgressByPhoneNumber(@PathVariable String phoneNumber) {
         return progressService.getProgressByPhoneNumber(phoneNumber);
@@ -23,6 +28,21 @@ public class ProgressController {
     @GetMapping("/get-ranking-page")
     public RankingPageResponse getRanking(){
         return progressService.getRanking();
+    }
+
+    @PostMapping("/create-discount{studentId}") // chegirma olish
+    public DiscountResponse createDiscount(@RequestBody DiscountRequest discountRequest, @PathVariable UUID studentId){
+        return discountService.create(discountRequest, studentId);
+    }
+
+    @GetMapping("/get-discounts-by-student{studentId}")
+    public List<DiscountResponse> getDiscountsByStudentId(@PathVariable UUID studentId){
+        return discountService.getDiscountsByStudentId(studentId);
+    }
+
+    @DeleteMapping("/delete-discount{discountId}")
+    public String deleteDiscount(@PathVariable UUID discountId){
+        return discountService.delete(discountId);
     }
 }
 
