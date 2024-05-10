6030f622cd650a62aa0f0fc89fde2c5effcaf110
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-05-10 19:52:35 +0500

    get-ranking-page

--- src/main/java/uz/rivoj/education/controller/ProgressController.java
+++ src/main/java/uz/rivoj/education/controller/ProgressController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.rivoj.education.dto.response.HomePageResponse;
+import uz.rivoj.education.dto.response.RankingPageResponse;
 import uz.rivoj.education.service.ProgressService;
 
 @RestController
@@ -13,9 +14,15 @@ import uz.rivoj.education.service.ProgressService;
 @RequestMapping("api/v1/progress")
 public class ProgressController {
     private final ProgressService progressService;
-    @GetMapping("get-progress/{phoneNumber}")
+    @GetMapping("/get-progress/{phoneNumber}")
     public HomePageResponse getProgressByPhoneNumber(@PathVariable String phoneNumber) {
         return progressService.getProgressByPhoneNumber(phoneNumber);
+    }
+    /* yana bir tekshirish kerak . Mening yo'nalishim va barcha guruhlar bo'yicha studentlar
+         reytingini ha olib kelish kerak ekan. Shularga kerakli veriablerni qoshish kerak 👇 */
+    @GetMapping("/get-ranking-page")
+    public RankingPageResponse getRanking(){
+        return progressService.getRanking();
     }
 }
 
