0c7c2b46841e726d54a3c288eab748d905cd99ec
Ulug'bek <kimsanov0717@gmail.com>
2024-05-27 11:52:47 +0500

    Prepare to deploy Add some dependencies

--- src/main/java/uz/rivoj/education/entity/ChatEntity.java
+++ src/main/java/uz/rivoj/education/entity/ChatEntity.java
@@ -13,7 +13,11 @@ import java.util.List;
 @Builder
 public class ChatEntity extends BaseEntity{
     @ManyToMany
-    @JoinColumn(name = "membersId", referencedColumnName = "id")
+    @JoinTable(
+            name = "chat_members",
+            joinColumns = @JoinColumn(name = "chat_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id")
+    )
     private List<UserEntity> members;
     @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> messages;
