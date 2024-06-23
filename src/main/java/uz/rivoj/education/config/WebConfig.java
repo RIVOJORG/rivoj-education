a7222193c40102f4745ab8360f5c3b2939038e03
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-06-23 20:43:17 +0500

    Cors

--- src/main/java/uz/rivoj/education/config/WebConfig.java
+++ src/main/java/uz/rivoj/education/config/WebConfig.java
@@ -8,23 +8,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://84.54.70.22:5174")  // Replace with your frontend's IP and port
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins for testing
-                .allowedMethods("*")
+                .allowedOrigins("http://84.54.70.22:5174")  // Replace with your frontend's IP and port
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")  // Allow all origins for testing
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 
 }
 
