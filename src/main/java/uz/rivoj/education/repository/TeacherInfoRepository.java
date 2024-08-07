6bb855b72ae573a6671af22721c9588d20bcf00e
JahongirDevelop <sjahongir221@gmail.com>
2024-08-07 13:09:12 +0500

    TeacherInfoRepository

--- src/main/java/uz/rivoj/education/repository/TeacherInfoRepository.java
+++ src/main/java/uz/rivoj/education/repository/TeacherInfoRepository.java
@@ -3,9 +3,11 @@ package uz.rivoj.education.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.rivoj.education.entity.StudentInfo;
+import uz.rivoj.education.entity.SubjectEntity;
 import uz.rivoj.education.entity.TeacherInfo;
 import uz.rivoj.education.entity.UserEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +17,5 @@ public interface TeacherInfoRepository extends JpaRepository<TeacherInfo, UUID> 
     Optional<TeacherInfo> findTeacherInfoBySubjectId(UUID id);
     TeacherInfo findByTeacher(UserEntity teacher);
 
+    List<TeacherInfo> findBySubject(SubjectEntity subject);
 }
