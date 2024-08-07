8f6a931a7568a0ae5a0e59d5067e3852a1bc9143
JahongirDevelop <sjahongir221@gmail.com>
2024-08-07 13:07:42 +0500

    ModuleRepository and AdminController

--- src/main/java/uz/rivoj/education/repository/ModuleRepository.java
+++ src/main/java/uz/rivoj/education/repository/ModuleRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import uz.rivoj.education.entity.ModuleEntity;
 import uz.rivoj.education.entity.SubjectEntity;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleEntity, UUID> {
@@ -18,4 +20,6 @@ public interface ModuleRepository extends JpaRepository<ModuleEntity, UUID> {
     Integer countBySubject(SubjectEntity subject);
     ModuleEntity findBySubjectAndNumber(SubjectEntity subject, Integer number);
 
+    Optional<ModuleEntity> findByNumberAndSubjectId(Integer moduleNumber, UUID subjectId);
+    Optional<List<ModuleEntity>> findAllBySubject(SubjectEntity subject);
 }
