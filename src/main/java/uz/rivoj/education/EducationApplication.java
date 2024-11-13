a4aa680ad9ffea66e81b817b00ff78687374e25c
Ulug'bek <140692404+UlugbekKimsanov@users.noreply.github.com>
2024-11-13 19:28:29 +0500

    Update EducationApplication.java

--- src/main/java/uz/rivoj/education/EducationApplication.java
+++ src/main/java/uz/rivoj/education/EducationApplication.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.FileNotFoundException;
 
 import java.io.File;
 import java.io.FileInputStream;
