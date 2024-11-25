9923b927066ff9021293e1b3297f14846ef61697
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-26 01:03:50 +0500

    add tokenRefresh

--- src/main/java/uz/rivoj/education/service/jwt/JwtUtil.java
+++ src/main/java/uz/rivoj/education/service/jwt/JwtUtil.java
@@ -30,7 +30,55 @@ public class JwtUtil {
 
     private final RedisTemplate<String, String> redisTemplate;
 
+    public List<String> generateToken(UserEntity user) {
+        Date iat = new Date();
+        String token = Jwts.builder()
+                .setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(iat)
+                .setExpiration(new Date(iat.getTime() + expiry))
+                .addClaims(getAuthorities(user))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
 
+        String oldToken = redisTemplate.opsForValue().get(user.getId().toString());
+        if (oldToken != null) {
+            redisTemplate.delete(oldToken);
+        }
+        String refreshToken = generateRefreshToken(user);
+        List<String> tokens = new ArrayList<>();
+        tokens.add(token);
+        tokens.add(refreshToken);
+        redisTemplate.opsForValue().set(String.valueOf(user.getId()), token);
+        return tokens;
+
+    }
+    public String generateRefreshToken(UserEntity user) {
+        Date iat = new Date();
+        String token = Jwts.builder()
+                .setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(iat)
+                .setExpiration(new Date(iat.getTime() + 1296000000L))
+                .addClaims(getAuthorities(user))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+        String oldToken = redisTemplate.opsForValue().get(user.getId() + "r");
+        System.out.println("Old Token = " + oldToken);
+        if (oldToken != null) {
+            redisTemplate.delete(oldToken);
+        }
+        System.out.println("New token = " + token);
+        redisTemplate.opsForValue().set(user.getId() + "r", token);
+        return token;
+    }
+    public boolean checkRefreshToken(UserEntity user, String token){
+        String oldRefreshToken = redisTemplate.opsForValue().get(user.getId() + "r");
+        Date expiration = extractToken(oldRefreshToken).getBody().getExpiration();
+        if(!expiration.after(new Date())){
+            return false;
+        }
+        assert oldRefreshToken != null;
+        return oldRefreshToken.equals(token);
+    }
 
     public Jws<Claims> extractToken(String token) {
         return Jwts.parserBuilder()
