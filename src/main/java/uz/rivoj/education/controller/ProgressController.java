cca2eb4d886ce1fb53ba2d54b5ed41ca28cd6b48
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-11-26 01:11:26 +0500

    add tokenRefresh

--- src/main/java/uz/rivoj/education/controller/ProgressController.java
+++ src/main/java/uz/rivoj/education/controller/ProgressController.java
@@ -17,6 +17,34 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/progress")
 public class ProgressController {
+    private final ProgressService progressService;
+    private final DiscountService discountService;
+
+    @GetMapping("/get-top10")
+    public RankingPageResponse getRankingPage(){
+        return progressService.getTop10Students();
+    }
+    @GetMapping("/get-top10-by-subject")
+    public RankingPageResponse getTop10StudentBySubject(Principal principal){
+        return progressService.getTop10StudentsBySubject(UUID.fromString(principal.getName()));
+    }
+
+
+    @PostMapping("/create-discount")
+    public DiscountResponse createDiscount(@RequestBody DiscountCR discountCR, Principal principal){
+        return discountService.create(discountCR, UUID.fromString(principal.getName()));
+    }
+
+    @GetMapping("/get-discounts-by-student")
+    public List<DiscountResponse> getDiscountsByStudentId(Principal principal){
+        return discountService.getDiscountsByStudentId(UUID.fromString(principal.getName()));
+    }
+
+    @DeleteMapping("/delete-discount{discountId}")
+    public String deleteDiscount(@PathVariable UUID discountId){
+        return discountService.delete(discountId);
+    }
+
 
 }
 
